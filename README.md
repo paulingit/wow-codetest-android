ee09cfc4e32f1ccd40359b879799c378315c6d37
Daniel Kang <vicfaith@nate.com>
2014-08-28 18:51:30 +1000

    update an optional requirement in README.md

--- README.md
+++ README.md
@@ -1,15 +1,17 @@
-WOW Code Test App
+WOW Code Test App (Updated)
 ---
-This app is a simple Android Studio project which displays fuel station list in NY.
+This app is a simple Android Studio project which displays fuel station list of NY.
 
 Fork this skeleton project or create your own project to make it complete as follows.
 
 * implement codes to display fuel station list from the server (URL is in the codes) and detail view as below
-* support orientation changes on Activity or Dialog without restarting network task (do not use configChanges in the AndroidManifest.xml)
+* support orientation changes on Activity without restarting network task (do not use configChanges in the AndroidManifest.xml)
 * implement codes for refresh button, proper title in action bar, home up button, etc
-* implement action menu to sort the data based on brand (BT, shell, etc)
 * download images in an asynchronous way
 * add more test cases for user codes
+
+[Optional]
+* implement action menu to sort the data based on brand (BT, shell, etc)
 
 You may use best Android practices to meet the requirements and modern open source libraries (networking, parser, event, etc) are preferrable for better app architecture.
 
--- app/src/main/java/au/com/wow/codetestapp/FuelStationListActivity.java
+++ app/src/main/java/au/com/wow/codetestapp/FuelStationListActivity.java
@@ -1,15 +1,9 @@
 package au.com.wow.codetestapp;
 
 import android.app.Activity;
-import android.app.AlertDialog;
-import android.content.DialogInterface;
-import android.database.DataSetObserver;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ListAdapter;
 
 public class FuelStationListActivity extends Activity {
 
@@ -34,79 +28,10 @@ public class FuelStationListActivity extends Activity {
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
         if (id == R.id.action_sortbybrand) {
-            populateBrandDialog();
+            //optional if you have enough time
+            //showDialog();
             return true;
         }
         return super.onOptionsItemSelected(item);
-    }
-
-    private void populateBrandDialog() {
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setAdapter(new ListAdapter() {
-            @Override
-            public boolean areAllItemsEnabled() {
-                return false;
-            }
-
-            @Override
-            public boolean isEnabled(int i) {
-                return false;
-            }
-
-            @Override
-            public void registerDataSetObserver(DataSetObserver dataSetObserver) {
-
-            }
-
-            @Override
-            public void unregisterDataSetObserver(DataSetObserver dataSetObserver) {
-
-            }
-
-            @Override
-            public int getCount() {
-                return 0;
-            }
-
-            @Override
-            public Object getItem(int i) {
-                return null;
-            }
-
-            @Override
-            public long getItemId(int i) {
-                return 0;
-            }
-
-            @Override
-            public boolean hasStableIds() {
-                return false;
-            }
-
-            @Override
-            public View getView(int i, View view, ViewGroup viewGroup) {
-                return null;
-            }
-
-            @Override
-            public int getItemViewType(int i) {
-                return 0;
-            }
-
-            @Override
-            public int getViewTypeCount() {
-                return 1;
-            }
-
-            @Override
-            public boolean isEmpty() {
-                return false;
-            }
-        }, new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
-
-            }
-        }).setMessage("Sort listings by brand").create().show();
     }
 }
