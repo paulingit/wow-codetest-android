96d125cf09fe5234eaea930a2d121df5d5313687
paulingit <paulg4321@gmail.com>
2016-03-13 05:54:05 +0530

    FuelStationListTest additional test cases.

--- app/src/androidTest/java/au/com/wow/codetestapp/FuelStationListTest.java
+++ app/src/androidTest/java/au/com/wow/codetestapp/FuelStationListTest.java
@@ -38,5 +38,21 @@ public class FuelStationListTest extends InstrumentationTestCase {
         assertEquals(mJsonObject.getJSONArray("item").length(), 50);
     }
 
-    // add more test cases
+    public void testZipValue() throws JSONException{
+        assertEquals(mJsonObject.getString("zip"),"10024");
+    }
+
+    public void testFuelStationItems() throws JSONException {
+        JSONArray jsonArray = mJsonObject.getJSONArray("item");
+        JSONObject jsonObject = jsonArray.getJSONObject(0);
+        assert(jsonObject.getString("regular").equals("4.07"));
+        assert(jsonObject.getString("plus").equals("false"));
+        assert(jsonObject.getString("premium").equals("4.47"));
+        assert(jsonObject.getString("diesel").equals("false"));
+        assert(jsonObject.getString("brand").equals("Mobil"));
+        assert(jsonObject.getString("img").equals("http:\\/\\/www.mshd.net\\/images\\/gas\\/mobil.gif"));
+        assert(jsonObject.getString("address").equals("303 W 96th St, New York, NY 10025"));
+        assert(jsonObject.getString("pupdate").equals("1408457760"));
+        assert(jsonObject.getString("distance").equals("0.8847563"));
+    }
 }
